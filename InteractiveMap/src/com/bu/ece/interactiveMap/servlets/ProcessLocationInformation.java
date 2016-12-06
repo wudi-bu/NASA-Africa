2a289094aa7f513c0104edc520b30f1c215c0f05
Anindya <akpaul@bu.edu>
2016-12-05 22:58:35 -0500

    Added soil deficiencies

--- InteractiveMap/src/com/bu/ece/interactiveMap/servlets/ProcessLocationInformation.java
+++ InteractiveMap/src/com/bu/ece/interactiveMap/servlets/ProcessLocationInformation.java
@@ -9,14 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
-
-import com.bu.ece.interactiveMap.cropPrediction.CropPrediction;
 import com.bu.ece.interactiveMap.cropPrediction.CropPredictionHelper;
 import com.bu.ece.interactiveMap.cropPrediction.PredictionResultBean;
-import com.bu.ece.interactiveMap.cropPrediction.SoilPropertyBean;
-import com.bu.ece.interactiveMap.soilgrid.SoilGridIntegration;
-import com.bu.ece.interactiveMap.utils.ESoilUtils;
 
 public class ProcessLocationInformation extends HttpServlet {
 
@@ -43,8 +37,44 @@ public class ProcessLocationInformation extends HttpServlet {
 				if(prediction != null) {
 					if(prediction.getIs_Cultivatible()) {
 						cropsPredicted = true;
-						System.out.println("Crop="+prediction.getCropName()+", Accuracy="+prediction.getAccuracy());
-						out.println("<h4>Crop="+prediction.getCropName()+", Accuracy="+prediction.getAccuracy()+"</h4>");
+						
+						ArrayList<String> soilDeficiencies = new ArrayList<String>();
+						StringBuilder deficiencies = new StringBuilder();
+						if(!prediction.getIs_Al_OK()) {
+							soilDeficiencies.add("Aluminium");
+							deficiencies.append("Aluminium, ");
+						}
+						if(!prediction.getIs_Ca_OK()) {
+							soilDeficiencies.add("Calcium");
+							deficiencies.append("Calcium, ");
+						}
+						if(!prediction.getIs_P_OK()) {
+							soilDeficiencies.add("Phosphorus");
+							deficiencies.append("Phosphorus, ");
+						}
+						if(!prediction.getIs_PH_OK()) {
+							soilDeficiencies.add("Ph");
+							deficiencies.append("Ph, ");
+						}
+						if(!prediction.getIs_Mg_OK()) {
+							soilDeficiencies.add("Magnesium");
+							deficiencies.append("Magnesium, ");
+						}
+						if(!prediction.getIs_Sand_OK()) {
+							soilDeficiencies.add("Sand");
+							deficiencies.append("Sand, ");
+						}
+						if(!prediction.getIs_SOC_OK()) {
+							soilDeficiencies.add("SOC");
+							deficiencies.append("SOC, ");
+						}
+						String deficienciesAsString = deficiencies.toString();
+						if(deficienciesAsString.length() > 2) {
+							deficienciesAsString = deficienciesAsString.substring(0, (deficienciesAsString.length() - 2));
+						}
+						
+						System.out.println("Crop="+prediction.getCropName()+", Accuracy="+prediction.getAccuracy()+", Deficiencies="+deficienciesAsString);
+						out.println("<h6>Crop="+prediction.getCropName()+", Accuracy="+prediction.getAccuracy()+", Deficiencies="+deficienciesAsString+"</h6>");
 					}
 				}
 			}
